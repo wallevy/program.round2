5495629b9850698b09cf31a9803648156e265937
walle <walle@evy.null>
2019-04-24 21:31:28 +0800

    p185: finish tdd task

--- p185/tdd/main/main.js
+++ p185/tdd/main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+  let sum = 6 + 0.25 * inputs.parkTime
+
+  if (inputs.distance > 2 && inputs.distance <= 8)
+    sum += 0.8 * (inputs.distance - 2)
+
+  if (inputs.distance > 8)
+    sum += 0.8 * 6 + 1.2 * (inputs.distance - 8)
+
+  return Math.round(sum)
 };
