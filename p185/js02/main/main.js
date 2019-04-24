ea6d63531f0e9e3936470d364baecc3695ec2108
walle <walle@evy.null>
2019-04-24 21:17:16 +0800

    p185: finish js02 task

--- p185/js02/main/main.js
+++ p185/js02/main/main.js
@@ -1,4 +1,25 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+const nums = [
+  ['._.', '|.|', '|_|'],
+  ['...', '..|', '..|'],
+  ['._.', '._|', '|_.'],
+  ['._.', '._|', '._|'],
+  ['...', '|_|', '..|'],
+  ['._.', '|_.', '._|'],
+  ['._.', '|_.', '|_|'],
+  ['._.', '..|', '..|'],
+  ['._.', '|_|', '|_|'],
+  ['._.', '|_|', '..|'],
+]
+
+
+module.exports = function main(str) {
+  const result = [[], [], []]
+
+  for (let c of str) {
+    result[0].push(nums[c][0])
+    result[1].push(nums[c][1])
+    result[2].push(nums[c][2])
+  }
+
+  return result.map(e => e.join(' ') + '\n').join('')
 };
